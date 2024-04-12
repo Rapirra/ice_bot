52f5f309866215b2efef3cc07eca1b7ad3e08e60
rapirra <anarzaurbek@gmail.com>
2024-04-12 18:42:15 +0500

    processing of buttons and entering token

--- handlers/pickup_handler.py
+++ handlers/pickup_handler.py
@@ -1,5 +1,5 @@
 from aiogram import Router, F, html
-from aiogram.filters import CommandStart, CommandObject, Command
+from aiogram.filters import CommandStart, CommandObject, Command, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message, CallbackQuery
@@ -11,8 +11,7 @@ router = Router(name=__name__)
 
 
 class RegisterMessages(StatesGroup):
-    userEmail = State()
-    userPassword = State()
+    userToken = State()
 
 
 class DB:
@@ -22,11 +21,22 @@ class DB:
 @router.message(CommandStart())
 async def process_start_command(message: Message, state: FSMContext):
     await message.answer('Enter your token', parse_mode="HTML")
+    await state.set_state(RegisterMessages.userToken)
 
 
-@router.message(F.text)
-async def extract_data(message: Message):
-    await message.answer(f"Your token {message.text}\n\n", parse_mode="HTML")
+@router.message(RegisterMessages.userToken, F.text)
+async def extract_data(message: Message, state: FSMContext):
+    await state.update_data(userToken=message.text.lower())
+    user_data = await state.get_data()
+    print(user_data)
+    await message.answer(
+        text=f"Your token {user_data['userToken']}."
+    )
+
+
+@router.message()
+async def processing_auto_message(msg: Message):
+    await bot.send_message(msg.chat.id, 'hi banshee')
 
 
 @router.callback_query(
